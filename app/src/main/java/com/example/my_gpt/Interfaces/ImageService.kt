7ffc71b60bb9cb7c671bb37d210cd3e355708e44
Deep-005 <162778410+Deep-005@users.noreply.github.com>
2025-01-04 22:14:08 -0800

    Update ImageService.kt

--- app/src/main/java/com/example/my_gpt/Interfaces/ImageService.kt
+++ app/src/main/java/com/example/my_gpt/Interfaces/ImageService.kt
@@ -12,7 +12,7 @@ data class NewImageResponse(val url: String)
 interface ImageService {
 
     @Headers(
-        "x-rapidapi-key: 6199e24496msh4ba9d031fa24763p12cd8ejsn665804ca23a4",
+        "x-rapidapi-key: 61########################################23a4",
         "x-rapidapi-host: ai-text-to-image-generator-api.p.rapidapi.com",
         "Content-Type: application/json"
     )
